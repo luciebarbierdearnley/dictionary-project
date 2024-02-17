dc983e80aac979b9ef2891b3dd74edde634dda04
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-17 11:18:49 +0000

    Added the initial text in the container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        Dictionary <br />
+        Enter a word...
         <Dictionary />
       </div>
       <footer>Coded by Lucie</footer>
