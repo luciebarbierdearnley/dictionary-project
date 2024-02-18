0ebdddcdf3a0a63a3a662b23367515e6e0387e0e
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-18 09:39:41 +0000

    Deleted the phonetic component and included return in results

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Meaning from "./Meaning.js";
-import Phonetic from "./Phonetic.js";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.data.word}</h2>
-        <Phonetic phonetic={props.results.data.phonetic} />
+        <h4>{props.results.data.phonetic}</h4>
         {props.results.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
