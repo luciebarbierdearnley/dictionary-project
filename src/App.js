4e5cc96ea8dff1d47dc72b19b0c6601b3c2580ca
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-18 09:18:10 +0000

    Edited the GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -11,15 +11,26 @@ export default function App() {
       </div>
       <footer>
         Coded by{" "}
-        <a href="https://github.com/luciebarbierdearnley">
+        <a
+          href="https://github.com/luciebarbierdearnley"
+          target="_blank"
+          rel="noreferrer">
           Lucie Barbier-Dearnley
         </a>
         . Open-sourced on{" "}
-        <a href="https://github.com/luciebarbierdearnley/dictionary-project">
+        <a
+          href="https://github.com/luciebarbierdearnley/dictionary-project"
+          target="_blank"
+          rel="noreferrer">
           GitHub
         </a>
         . Hosted on{" "}
-        <a href="https://dictionary-app-lbd.netlify.app/">Netlify</a>
+        <a
+          href="https://dictionary-app-lbd.netlify.app/"
+          target="_blank"
+          rel="noreferrer">
+          Netlify
+        </a>
       </footer>
     </div>
   );
